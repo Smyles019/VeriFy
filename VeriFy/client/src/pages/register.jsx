e9c77124c1a484f48436a05fd2c6e68fcdb1fbf4
Smyles019 <168530329+Smyles019@users.noreply.github.com>
2025-06-23 10:08:23 +0300

    combined files and fixed the authentication system

--- VeriFy/client/src/pages/register.jsx
+++
@@ -1,6 +0,0 @@
-import RegisterForm from "../components/registerform.jsx";
-const Registration = () => {
-  return <RegisterForm />;
-};
-
-export default Registration;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,19 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Home from './pages/Home'
-import ArticleDetails from './pages/ArticleDetails'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import NotFound from './pages/NotFound'
-import Navbar from './components/Navbar';
-import NewsSection from './components/NewsSection';
-import CategorySlider from './components/CategorySlider';
-import Footer from './components/Footer';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
-function App(){
-    return(
-   <Router>
-     <div className="min-h-screen flex flex-col">
-      <Navbar/>
-       <main className='flex-grow px-6'>
-    <Routes>
-      <Route path="/" element = {
-        <>
-        <NewsSection/>
-        <CategorySlider/>
-        </>
-      }/>
-      <Route path="/" element={<Home />} />
-      <Route path="/article/:id" element={<ArticleDetails />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-    </main>
-    <Footer />
-    </div>
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<h1 className="text-2xl p-5">Welcome to VeriFy</h1>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<h1 className="text-2xl p-5">404 Not Found</h1>} />
+      </Routes>
     </Router>
-    ); 
+  );
 }
 
-
-export default App
+export default App;

a70cb7bb949c7deeddaa3a075e375896ae0ef035
cjsinari <cyrilsinari@gmail.com>
2025-07-02 20:17:32 +0300

    Added categorization to articles

--- frontend/src/components/NewsSection.jsx
+++ frontend/src/components/NewsSection.jsx
@@ -1,33 +1,9 @@
-import React from "react"
+//import { useState, useEffect} from "react"
+import axios from "axios"
 import ArticleCard from "./ArticleCard"
+ 
+const NewsSection = ({ title, articles }) => {
 
-  const articles = [
-    {
-        category: "Trending",
-        title: "Israel kills 23 Palestinians in Gaza",
-        author: "Terry Washington",
-        date: "15 June 2025",
-        description: "Lorem ipsum dolor"
-    },
-
-    {
-    category: "Politics",
-    title: "Governor of Salem County arrested over fraud charges",
-    author: "John Doe",
-    date: "13 June 2025",
-    description: "Nunc vulputate efficitur tortor sit amet sodales. Aenean pharetra ut mauris sit amet commodo...",
-  },
-  {
-    category: "Business",
-    title: "Deep dive into Kenya's budget for 2025/2026",
-    author: "Freddy Nomics",
-    date: "14 June 2025",
-    description: "Nunc vulputate efficitur tortor sit amet sodales. Aenean pharetra ut mauris sit amet commodo...",
-  },
-
-  ]
-    
-const NewsSection = () => {
     return(
       <section className="px-8 py-6">
          <h2 className="text-2xl font-bold mb-6">Latest</h2>
@@ -35,18 +11,18 @@ const NewsSection = () => {
             {articles.map((article, index) => (
               <div key={index} className="bg-white rounded-lg shadow p-4 flex flex-col">
                 <div className="h-32 bg-gray-200 rounded mb-4"></div>  {/*Placeholder image*/}
-                <p className="text-xs text-zinc-950 font-semibold uppercase">{article.category}</p>
+                <p className="text-xs text-zinc-950 font-semibold uppercase">{article.category || article.tags?.[0]}</p>
                 <h2 className="font-bold text-lg mb-1">{article.title}</h2>
                 <div className="text-xs text-zinc-950 mb-2 flex justify-between">
-                    <span>{article.author}</span>
-                    <span>{article.date}</span>
+                    <span>{article.submittedBy}</span>
+                    <span>{new Date(article.createdAt).toLocaleDateString('en-GB')}</span>
                 </div>
-                <p className="text-sm text-zinc-950">{article.description}</p>            
+                <p className="text-sm text-zinc-950">
+                  {article.content.replace(/<[^>]+>/g, '').slice(0, 100)}...
+                </p>            
               </div>             
              ))}
-
         </div>
-
       </section>  
     )
 }

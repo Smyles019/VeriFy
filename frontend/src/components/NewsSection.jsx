8a6bc2eeab5cc740e93bfe4e537481da54edf473
cjsinari <cyrilsinari@gmail.com>
2025-07-04 00:18:55 +0300

    Added thumbnails to article cards

--- frontend/src/components/NewsSection.jsx
+++ frontend/src/components/NewsSection.jsx
@@ -1,5 +1,6 @@
 //import { useState, useEffect} from "react"
 import axios from "axios"
+import { Link } from "react-router-dom"
 import ArticleCard from "./ArticleCard"
  
 const NewsSection = ({ title, articles }) => {
@@ -9,8 +10,16 @@ const NewsSection = ({ title, articles }) => {
          <h2 className="text-2xl font-bold mb-6">Latest</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
             {articles.map((article, index) => (
-              <div key={index} className="bg-white rounded-lg shadow p-4 flex flex-col">
-                <div className="h-32 bg-gray-200 rounded mb-4"></div>  {/*Placeholder image*/}
+              <Link to={`/article/${article._id}`} key={index} className="bg-white rounded-lg shadow p-4 flex flex-col hover:shadow-lg transition-shadow" >
+                {article.image ? (
+                  <img 
+                     src = {`http://localhost:5000/uploads/${article.image}`}
+                     alt={article.title}
+                     className="h-32 w-full object-cover rounded mb-4" />
+                ) : (
+                  <div className="h-32 bg-gray-200 rounded mb-4"></div>
+               
+                )}
                 <p className="text-xs text-zinc-950 font-semibold uppercase">{article.category || article.tags?.[0]}</p>
                 <h2 className="font-bold text-lg mb-1">{article.title}</h2>
                 <div className="text-xs text-zinc-950 mb-2 flex justify-between">
@@ -19,8 +28,8 @@ const NewsSection = ({ title, articles }) => {
                 </div>
                 <p className="text-sm text-zinc-950">
                   {article.content.replace(/<[^>]+>/g, '').slice(0, 100)}...
-                </p>            
-              </div>             
+                </p>
+                </Link>            
              ))}
         </div>
       </section>  
